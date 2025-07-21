5e2515b15e23bdc0eee4f1741a2ae861fe7ea99f
mindchuu <chutathiptermchaikul@gmail.com>
2025-07-21 09:57:09 +0700

    Update ch3_3.py

--- ch3/ch3_3.py
+++ ch3/ch3_3.py
@@ -25,18 +25,33 @@ def in_to_postfix(str):
     index = 0
     result = ''
     operation = {'(':3, '{':3, '[':3, '^':2, '*':1, '/':1, '+':0, '-':0}
-
     while index < len(str):
         c = str[index]
-        if c in '+-*/^({[':
-            if s.isEmpty():
-                s.push(c)
-            while operation[c] <= operation[s.peek()] \
-                and s.peek() != '({[':
+        if c in '({[':
+            s.push(c)
+        elif c in ')}]':
+            if c == ')':
+                while s.peek() != '(':
+                    result += s.pop()
+            if c == '}':
+                while s.peek() != '{':
+                    result += s.pop()
+            if c == ']':
+                while s.peek() != '[':
+                    result += s.pop()
+            s.pop()
+        elif c in '+-*/^':
+            while s.isEmpty() == False \
+                and s.peek() not in '({[' \
+                and operation[c] <= operation[s.peek()]:
                 result += s.pop()
+            s.push(c)
         else:
             result += c
-        
-
+        index += 1
+    while s.isEmpty() == False:
+        result += s.pop()
+    return result
 
 inp = input("Enter Infix : ")
+print(f"Postfix : {in_to_postfix(inp)}")
